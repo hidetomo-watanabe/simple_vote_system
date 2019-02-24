4c18e30ea9951d1e828de038410376ea7fe1676b
hidetomo-watanabe <attyamunn@gmail.com>
2019-02-24 09:16:10 +0900

    アイテムがない時の画面追加

--- simple_vote_system/src/components/Item.vue
+++ simple_vote_system/src/components/Item.vue
@@ -1,27 +1,33 @@
 <template>
-  <table>
-    <tr>
-      <td class="item" v-for="(item, index) in items" :key="index">
-        <h4>{{item.name}}</h4>
-        <div class="referee">{{item.referee}}</div>
-      </td>
-    </tr>
-    <tr>
-      <td class="item" v-for="(item, index) in items" :key="index">
-        <img :src="getImgPath(item.imgPath)" :alt="item.name" :title="item.name">
-      </td>
-    </tr>
-    <tr>
-      <td class="item" v-for="(item, index) in items" :key="index">
-        <div class="count">
-          <button v-on:click="increment(item.id, item.count)">
-            <img src="../assets/good.png" alt="good" title="good">
-          </button>
-          {{item.count}}
-        </div>
-      </td>
-    </tr>
-  </table>
+  <ul v-if="items.length">
+    <table>
+      <tr>
+        <td class="item" v-for="(item, index) in items" :key="index">
+          <h4>{{item.name}}</h4>
+          <div class="referee">{{item.referee}}</div>
+        </td>
+      </tr>
+      <tr>
+        <td class="item" v-for="(item, index) in items" :key="index">
+          <img :src="getImgPath(item.imgPath)" :alt="item.name" :title="item.name">
+        </td>
+      </tr>
+      <tr>
+        <td class="item" v-for="(item, index) in items" :key="index">
+          <div class="count">
+            <button v-on:click="increment(item.id, item.count)">
+              <img src="../assets/good.png" alt="good" title="good">
+            </button>
+            {{item.count}}
+          </div>
+        </td>
+      </tr>
+    </table>
+  </ul>
+  <ul v-else>
+    <h2>アイテムを追加してください</h2>
+    <br><br>
+  </ul>
 </template>
 
 <script>
